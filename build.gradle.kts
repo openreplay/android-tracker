746c1f05f08f142422238b071d0085f421cc8240
Shekar Siri <sshekarsiri@gmail.com>
2024-04-17 14:17:50 +0200

    publish changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,13 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-    }
+        maven {
+            url = uri("https://jitpack.io")
+        }
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        dependencies {
+            classpath("com.android.tools.build:gradle:8.2.2")
+            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        }
     }
 }
