4aaac5686b6d8939a1e31dbfb971f6c2ec57acb9
Shekar Siri <sshekarsiri@gmail.com>
2024-11-28 11:14:14 +0100

    fix(tracker): performance monitor

--- tracker/src/main/java/com/openreplay/tracker/listeners/PerformanceListener.kt
+++ tracker/src/main/java/com/openreplay/tracker/listeners/PerformanceListener.kt
@@ -6,52 +6,44 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
-import androidx.lifecycle.*
-import com.openreplay.tracker.managers.MessageCollector
-import java.util.Timer
-import java.util.TimerTask
-import android.os.Process
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.openreplay.tracker.OpenReplay
 import com.openreplay.tracker.managers.DebugUtils
+import com.openreplay.tracker.managers.MessageCollector
 import com.openreplay.tracker.models.script.ORMobilePerformanceEvent
-import java.io.File
+import java.lang.ref.WeakReference
 import kotlin.math.round
 
-class PerformanceListener private constructor(private val context: Context) : DefaultLifecycleObserver {
+class PerformanceListener private constructor(applicationContext: Context) :
+    DefaultLifecycleObserver {
+
     companion object {
-        var isActive: Boolean = false
+        @Volatile
         private var instance: PerformanceListener? = null
 
-        fun getInstance(applicationContext: Context): PerformanceListener =
+        fun getInstance(context: Context): PerformanceListener =
             instance ?: synchronized(this) {
-                instance ?: PerformanceListener(applicationContext.applicationContext).also { instance = it }
+                instance ?: PerformanceListener(context.applicationContext).also { instance = it }
             }
-
-        fun networkStateChange(i: Int) {
-            // Handle network state change
-            println("Network state change: $i")
-        }
     }
 
+    private val appContext = applicationContext.applicationContext
     private var batteryLevelReceiver: BroadcastReceiver? = null
-    private var cpuTimer: Timer? = null
-    private var memTimer: Timer? = null
-    private var performanceTimer: Timer? = null
-
-    init {
-        // Example of registering to lifecycle events if the context is an Activity or Fragment
-        (context as? LifecycleOwner)?.lifecycle?.addObserver(this)
-    }
+    private val cpuHandler = Handler(Looper.getMainLooper())
+    private val memoryHandler = Handler(Looper.getMainLooper())
+    private var lifecycleOwnerRef: WeakReference<LifecycleOwner>? = null
+    private var isActive: Boolean = false
 
     override fun onResume(owner: LifecycleOwner) {
         super.onResume(owner)
-
+        lifecycleOwnerRef = WeakReference(owner)
         if (OpenReplay.options.debugLogs) {
             DebugUtils.log("Resume")
         }
-
         MessageCollector.sendMessage(ORMobilePerformanceEvent(name = "background", value = 0u))
-
         start()
     }
 
@@ -66,176 +58,102 @@ class PerformanceListener private constructor(private val context: Context) : De
 
     fun start() {
         if (!isActive) {
-            reportCpuUsage()
-            reportMemoryUsage()
-//            registerBatteryLevelReceiver()
-            setupTimers()
-//            setupPerformanceMonitoring()
+            registerBatteryLevelReceiver()
+            scheduleCpuUsageTask()
+            scheduleMemoryUsageTask()
             isActive = true
         }
     }
 
-//    private fun setupPerformanceMonitoring() {
-//        performanceTimer = Timer().apply {
-//            scheduleAtFixedRate(object : TimerTask() {
-//                override fun run() {
-////                    Log.d("Performance", "Checking performance metrics")
-//                    reportCpuUsage()
-//                    reportMemoryUsage()
-//                }
-//            }, 0, 2000) // Measure every 5 seconds
-//        }
-//    }
+    private fun registerBatteryLevelReceiver() {
+        if (batteryLevelReceiver == null) {
+            batteryLevelReceiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context, intent: Intent) {
+                    val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+                    val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+                    val batteryPct = level * 100 / scale.toFloat()
+                    println("Battery level: $batteryPct%")
+                }
+            }
+            appContext.registerReceiver(
+                batteryLevelReceiver,
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            )
+        }
+    }
+
+    private fun scheduleCpuUsageTask() {
+        cpuHandler.postDelayed(object : Runnable {
+            override fun run() {
+                reportCpuUsage()
+                cpuHandler.postDelayed(this, 10000)
+            }
+        }, 10000)
+    }
+
+    private fun scheduleMemoryUsageTask() {
+        memoryHandler.postDelayed(object : Runnable {
+            override fun run() {
+                reportMemoryUsage()
+                memoryHandler.postDelayed(this, 10000)
+            }
+        }, 10000)
+    }
 
     private fun reportCpuUsage() {
         val cpuUsage = calculateCpuUsage()
         println("CPU Usage: $cpuUsage%")
-
-        val message = ORMobilePerformanceEvent(name = "mainThreadCPU", value = cpuUsage.toULong())
-        MessageCollector.sendMessage(message)
+        MessageCollector.sendMessage(
+            ORMobilePerformanceEvent(name = "mainThreadCPU", value = cpuUsage.toULong())
+        )
     }
 
     private fun reportMemoryUsage() {
         val memoryUsage = memoryUsage()
         println("Memory Usage: $memoryUsage MB")
-        val message = ORMobilePerformanceEvent(name = "memoryUsage", value = memoryUsage.toULong())
-        MessageCollector.sendMessage(message)
+        MessageCollector.sendMessage(
+            ORMobilePerformanceEvent(name = "memoryUsage", value = memoryUsage.toULong())
+        )
     }
 
-    private var dummyResult: Double = 0.0
     private fun calculateCpuUsage(): Double {
         val startTime = System.nanoTime()
         var result = 0.0
         for (i in 0 until 1000000) {
             result += (i * i) % 1234567
         }
-        dummyResult = result  // Assign to a volatile field to prevent optimization
-
         val endTime = System.nanoTime()
-        val durationMs = (endTime - startTime) / 1_000_000.0  // Convert nanoseconds to milliseconds
+        val durationMs = (endTime - startTime) / 1_000_000.0
         return round(durationMs * 10) / 10.0
     }
 
-    fun getMemoryMessage() {
-        val memory = memoryUsage()
-        val message = ORMobilePerformanceEvent(name = "memoryUsage", value = memory.toULong())
-        MessageCollector.sendMessage(message)
-    }
-
-    private fun registerBatteryLevelReceiver() {
-        batteryLevelReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-                val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-                val batteryPct = level * 100 / scale.toFloat()
-                // Handle battery level change
-                println("Battery level: $batteryPct%")
-            }
-        }
-        IntentFilter(Intent.ACTION_BATTERY_CHANGED).also { ifilter ->
-            context.registerReceiver(batteryLevelReceiver, ifilter)
-        }
-    }
-
-    private fun setupTimers() {
-        cpuTimer = Timer().apply {
-            schedule(object : TimerTask() {
-                override fun run() {
-                    reportCpuUsage()
-                }
-            }, 0, 10000) // Every 5 seconds
-        }
-
-        memTimer = Timer().apply {
-            schedule(object : TimerTask() {
-                override fun run() {
-                    reportMemoryUsage()
-                }
-            }, 0, 10000) // Every 10 seconds
-        }
-    }
-
-    fun sendBattery() {
-        val message = ORMobilePerformanceEvent(name = "Battery", value = 20u)
-        MessageCollector.sendMessage(message)
-    }
-
-    fun sendThermalEvent() {
-        val message = ORMobilePerformanceEvent(name = "Thermal", value = 2u)
-        MessageCollector.sendMessage(message)
-    }
-
-//    private fun getCpuInfo(): CpuInfo? {
-//        File("/proc/stat").useLines { lines ->
-//            val cpuLine = lines.firstOrNull { it.startsWith("cpu ") } ?: return null
-//            val parts = cpuLine.split("\\s+".toRegex()).drop(1).mapNotNull { it.toLongOrNull() }
-//            if (parts.size >= 8) {
-//                val total = parts.sum()
-//                return CpuInfo(total)
-//            }
-//            return null
-//        }
-//    }
-
-//    private fun getAppCpuTime(pid: Int): Long {
-//        File("/proc/$pid/stat").useLines { lines ->
-//            val statLine = lines.firstOrNull() ?: return 0
-//            val parts = statLine.split("\\s+".toRegex())
-//            if (parts.size >= 17) {
-//                val utime = parts[13].toLongOrNull() ?: 0
-//                val stime = parts[14].toLongOrNull() ?: 0
-//                return utime + stime
-//            }
-//            return 0
-//        }
-//    }
-
-//    data class CpuInfo(val total: Long)
-//
-//    private fun cpuUsage(): Double? {
-//        try {
-//            val pid = Process.myPid()
-//            val cpuInfo1 = getCpuInfo()
-//            val appCpuTime1 = getAppCpuTime(pid)
-//
-//            Thread.sleep(1000) // Sleep for a second
-//
-//            val cpuInfo2 = getCpuInfo()
-//            val appCpuTime2 = getAppCpuTime(pid)
-//
-//            if (cpuInfo1 == null || cpuInfo2 == null) return null
-//
-//            val totalCpuTimeDelta = cpuInfo2.total - cpuInfo1.total
-//            val appCpuTimeDelta = appCpuTime2 - appCpuTime1
-//
-//            return if (totalCpuTimeDelta > 0) 100.0 * appCpuTimeDelta / totalCpuTimeDelta else null
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            return null
-//        }
-//    }
-
     private fun memoryUsage(): Long {
-        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+        val activityManager =
+            appContext.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
         val memoryInfo = ActivityManager.MemoryInfo()
         activityManager?.getMemoryInfo(memoryInfo)
 
         val runtime = Runtime.getRuntime()
-        val usedMemInMB = (runtime.totalMemory() - runtime.freeMemory()) / 1048576L
-        // val maxHeapSizeInMB = runtime.maxMemory() / 1048576L
-        // val availHeapSizeInMB = maxHeapSizeInMB - usedMemInMB
-        return usedMemInMB
+        return (runtime.totalMemory() - runtime.freeMemory()) / 1048576L
     }
 
     fun stop() {
         if (isActive) {
-//            context.unregisterReceiver(batteryLevelReceiver)
-//            batteryLevelReceiver = null
-            cpuTimer?.cancel()
-            cpuTimer = null
-            memTimer?.cancel()
-            memTimer = null
+            unregisterBatteryLevelReceiver()
+            cpuHandler.removeCallbacksAndMessages(null)
+            memoryHandler.removeCallbacksAndMessages(null)
             isActive = false
+        }
+    }
+
+    private fun unregisterBatteryLevelReceiver() {
+        try {
+            batteryLevelReceiver?.let {
+                appContext.unregisterReceiver(it)
+                batteryLevelReceiver = null
+            }
+        } catch (e: IllegalArgumentException) {
+            println("Battery level receiver was not registered or already unregistered.")
         }
     }
 }
